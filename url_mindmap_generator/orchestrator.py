ee1b2194fa631af0c94eb74a21b05a42fda9fad0
Guru Charan Bulusu <gurucb@microsoft.com>
2024-09-06 19:34:37 +0530

    Completed Web Scapper module

--- url_mindmap_generator/orchestrator.py
+++ url_mindmap_generator/orchestrator.py
@@ -11,9 +11,11 @@ class Orchestrator():
     content_json="{\"page_summary\":\"FullPageSummaryofthegivenURL\",\"content\":\"content\",\"topics\":[{\"name\":\"firsttopic\",\"sub_topics\":[{\"name\":\"firstsubtopic\",\"text\":\"Firstcsubtopictext\"},{\"name\":\"firstsubtopic\",\"text\":\"Firstcsubtopictext\"},{\"name\":\"firstsubtopic\",\"text\":\"Firstcsubtopictext\"}]},{\"name\":\"Secondtopic\",\"sub_topics\":[{\"name\":\"SecondFirstsubtopic\",\"text\":\"SecondFirstcsubtopictext\"},{\"name\":\"Secondsecondsubtopic\",\"text\":\"Secondsecondtopictext\"},{\"name\":\"SecondThirdsubtopic\",\"text\":\"SecondThirdtopictext\"}]}],\"URLS\":[\"www.Microsoft.com\",\"www.Google.com\",\"www.AWS.com\"]}"
 
     anchor_json = {}
+    url_json = {}
+    content = None
     def generate_mindmap(self,url, user_prompt):
         scrapper_object = ws.web_scrapper()
-        head_json  = scrapper_object.scrape_website(url)
+        head_json,url_json, content  = scrapper_object.scrape_website(url)
 
         # ai_skills = ai.create_content(head_json, user_prompt, web_content)
         # ai_skills.generate_subtopics(head_json, user_prompt)
@@ -22,3 +24,5 @@ class Orchestrator():
 if __name__ == "__main__":
     orchestrator_object =Orchestrator()
     orchestrator_object.generate_mindmap("https://learn.microsoft.com/en-us/azure/well-architected/reliability/principles","test")
+    # orchestrator_object.generate_mindmap("https://en.wikipedia.org/wiki/Microsoft","tst")
+    # 'http://www.nytimes.com/2009/12/21/us/21storm.html'
--- url_mindmap_generator/web_scrapper.py
+++ url_mindmap_generator/web_scrapper.py
@@ -4,12 +4,17 @@
 # 2. Anchors as JSON
 
 from bs4 import BeautifulSoup, ResultSet
+from bs4.element import Comment
 import requests
+import urllib
 import json
+import os
 
 class web_scrapper():
     soup = None
-    BeautifulSoup
+    content = None
+    headings_json = {}
+    page_links = []
     def scrape_website(self,url):
         # Check if it is valid URL
         assert(len(url) > 0)
@@ -18,37 +23,91 @@ class web_scrapper():
         self.response = requests.get(url=url)
         self.soup = BeautifulSoup(self.response.content,"html.parser")
         self.soup.builder = "lxml"
-        head_json = self.extract_headers()
-        anchors_json = self.extract_anchors(url)
-        
-        
-        return head_json, anchors_json
+        self.headings_json = self.extract_headers()
+        self.page_links = self.extract_anchors(url)
+        self.clean_headers()
+        self.content = self.extract_all_content(url)
+        self.content = self.content.replace(" "," ")
+        return self.headings_json, self.page_links, self.content
 
     def extract_headers(self ):
-        headers = []
-        self.h1 = self.soup.h1
-        h1 = {}
-        h1["h_type"]  = self.h1.name
-        h1["text"] = self.h1.contents
-        headers.append(h1)
-        self.h2 = self.soup.find_all("h2",recursive=True)
-        for h in self.h2:
-            h2 = {}
-            h2["h_type"] =  h.name 
-            h2["text"] = h.contents             
-            headers.append(h2)
-            h2 = None
-        head_json = json.dumps(headers)
-        return head_json
+        headers = {}
+        h1 = []
+        h2 = []
+        h1s = self.soup.find_all("h1",recursive=True)
+        for item in h1s:
+            heading_1 = {}
+            heading_1['h_type']  = item.name
+            heading_1['id'] = item.id
+            heading_1['text'] = item.contents
+            h1.append(heading_1)
+            heading_1=None
+        headers['h1'] = h1
+        h2s = self.soup.find_all("h2",recursive=True)
+        for item in h2s:
+            heading_2 = {}
+            heading_2['h_type'] =  item.name
+            heading_2['id'] = item.id 
+            heading_2['text'] = item.contents             
+            h2.append(heading_2)
+            heading_2 = None
+        headers["h2"] = h2
+        return headers
     def extract_anchors(self,url):
-        anchors = {}
+        anchors = []
+        links = self.soup.find_all('a',recursive=True)
+        for  link in links:
+            anchor =  {}
+            anchor["name"] = link.name
+            anchor["text"] = link.get_text()
+            if link.get('href') != None:
+                if 'https://' in link.get('href'):
+                    anchor["href"] = link.get_text()
+                else:
+                    anchor["href"] = "None"
+            anchor['attrs'] = link.attrs
+
+            anchors.append(anchor)
+            anchor = None
         return anchors
+    def is_noise_anchor(self,url) -> bool:
+        is_noise_anchor = False
     def clean_anchors(self,url):
         pass
-    def extract_all_content(self, url):
-        pass
+    def tag_visible(self,element):
+        if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+            return False
+        if isinstance(element, Comment):
+            return False
+        return True
+    def extract_all_content(self,url):
+        html = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(html, "html.parser")
+        texts = soup.findAll(text=True)
+        # Filter out unwanted tags
+        visible_texts = filter(self.tag_visible, texts)  
+        return u" ".join(t.strip() for t in visible_texts)
     def clean_response(self,url):
         pass
 
-    def clean_headers():
-        pass
+    # TODO: Need better implementation 
+    # Cleanup further like remove Tags
+    # using Word2Vec
+    def clean_headers(self):
+        json_cleaned = {}
+        h1s = list(self.headings_json['h1'])
+        h1s_orig = h1s
+        h2s = list(self.headings_json['h2'])
+        h2s_orig = h2s
+        stop_words = ['article','feedback','next','resources','see','references','external','notes']
+        for word in stop_words:
+            # for h1 in h1s_orig:
+            #     if str.lower(h1['text'][0]).find(str.lower(word)) != -1:
+                    # h1s.remove(h1)
+            for h2 in h2s_orig:
+                if str.lower(h2['text'][0]).find(str.lower(word)) != -1:
+                    h2s.remove(h2)
+        
+        self.headings_json['h1'] = h1s
+        self.headings_json['h2'] = h2s
+        
