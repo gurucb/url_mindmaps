1b7011f93a27afb5793e6f01cc3e9230cd5ce96b
Guru Charan Bulusu <gurucb@microsoft.com>
2024-09-07 08:36:10 +0530

    Added AI_Orchestrator_guru.

---
+++ url_mindmap_generator/ai_orchestrator_guru.py
@@ -0,0 +1,20 @@
+import json
+
+class AI_Orchstrator:
+    def __init__(self) -> None:
+        pass
+
+    def parse_content(self,heading_json,links_json,content) -> json:
+        pass
+
+    def generate_page_summary(self):
+        pass
+
+    def generate_topics_with_summary(self):
+        pass
+
+    def generate_subtopics_with_summary(self):
+        pass
+    
+    def generate_response_json(self):
+        pass
